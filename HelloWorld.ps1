d17cbcfeb8d0723add871582394ff29f2bf495b1
minalissac <45775088+minalissac@users.noreply.github.com>
2019-01-10 13:46:57 -0600

    Update HelloWorld.ps1

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -2,6 +2,6 @@ Param(
 [string]$greeter,
 [string]$trigger
 )
-Write-Host "Hello worldx" from $greeter
+Write-Host "Hello worldxxxx" from $greeter
 Write-Host Trigger: $trigger
 Write-Host "Now that you've got CI/CD, you can automatically deploy your app every time your team checks in code."
